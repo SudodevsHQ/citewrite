4d2f1adb89d46d4704ac171e71dabe5dd11766d2
Anurag Patil <anurag@sudodevs.com>
2019-10-05 23:23:34 +0530

    nice things

--- main.js
+++ main.js
@@ -3,10 +3,30 @@
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const contextMenu = require('electron-context-menu');
+
 
 
 var mainWindow = null;
 
+
+contextMenu({
+	prepend: (defaultActions, params, browserWindow) => [
+		{
+			label: 'Rainbow',
+			// Only show it when right-clicking images
+			visible: params.mediaType === 'image'
+		},
+		{
+			label: 'Search Google for “{selection}”',
+			// Only show it when right-clicking text
+			visible: params.selectionText.trim().length > 0,
+			click: () => {
+				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
+			}
+		}
+	]
+});
 app.on('window-all-closed', function() {
 	if (process.platform != 'darwin') {
 		app.quit();
@@ -17,7 +37,10 @@ app.on('ready', function() {
 	mainWindow = new BrowserWindow({
 		'minWidth': 800,
 		'minHeight': 600,
-		"frame":false
+		"frame":false,
+webPreferences: {
+	nodeIntegration: true
+	}
 	});
 	mainWindow.maximize();
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
@@ -25,3 +48,4 @@ app.on('ready', function() {
 		mainWindow = null;
 	});
 });
+
